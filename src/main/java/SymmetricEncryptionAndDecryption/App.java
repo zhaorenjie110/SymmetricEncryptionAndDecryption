9ee39926f88e102c615fda2e7695a984f0871605
zhaorenjie <zhaorenjie110@gmail.com>
2015-09-16 11:10:40 +0800

    Initial commit

---
+++ src/main/java/SymmetricEncryptionAndDecryption/App.java
@@ -0,0 +1,13 @@
+package SymmetricEncryptionAndDecryption;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
